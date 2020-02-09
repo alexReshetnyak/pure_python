2ba2d32874014ba82a7ecdd45456777d909bec99
A Reshetnyak <alexreshetnyakmail@gmail.com>
2020-02-09 18:57:19 +0200

    init

---
+++ 1_fundamentals/1_data_types.py
@@ -0,0 +1,32 @@
+# Fundamental Data types
+# int - all numbers
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes -> custom types
+
+# Specialized Data types
+
+# None (null, underfined)
+None
+
+# -----------------------------------------------------------------------------
+# Numbers
+print(1 + 2)
+print(type(2)) # int
+print(type(8 / 44)) #float
+print(2**3) # 8
+
+# Double divide (square root)
+print(4 // 2) # 2
+print(9 // 3) # 3
+
+# module
+print(6 % 4) # 2
+
