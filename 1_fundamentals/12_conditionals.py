a920be7eacbf98e89a63cb9128f7725b31465dbc
A Reshetnyak <alexreshetnyakmail@gmail.com>
2020-02-15 16:30:15 +0200

    worked on conditionals

---
+++ 1_fundamentals/12_conditionals.py
@@ -0,0 +1,13 @@
+print('---------------------------CONDITIONALS--------------------------------')
+is_old = False
+is_licenced = True
+
+if is_old and is_licenced:
+  print("'You are old!':", 'You are old!') # 'You are old!'
+elif is_licenced or is_old:
+  print("'You licenced already':", 'You licenced already') # 'You licenced already' 
+else:
+  print("'You are yong':", 'You are yong') # 'You are yong'
+
+
+print(":", ) # 
