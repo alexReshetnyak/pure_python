abf50d96405d9959cbf4f473ff68ea45b04add2b
A Reshetnyak <alexreshetnyakmail@gmail.com>
2020-02-16 21:33:51 +0200

    learned new details about loops

---
+++ 1_fundamentals/14_range .py
@@ -0,0 +1,24 @@
+print('---------------------------RANGE--------------------------------')
+# ? Object that produce a sequence of integers from start to stop
+
+print("range(100):", range(100)) # range(0, 100)
+
+for number in range(0,5):
+  print("number:", number) # 0,1,2,3,4,5
+
+print(":", ) # 
+
+for _ in range(0,5):
+  print(":", 'send email') # use _ if you don't need variable
+
+print(":", ) # 
+
+for number in range(0,10,2):
+  print("number:", number) # 0,2,4,6,8
+
+print(":", ) # 
+
+for number in range(10,0,-2): # back count
+  print("number:", number) # 10, 8, 6, 4, 2
+
+print(":", ) # 
---
+++ 1_fundamentals/15_enumerate.py
@@ -0,0 +1,12 @@
+print('---------------------------ENUMERATE--------------------------------')
+# ? Enumerate object, used if you need index while loop
+
+for i, char in enumerate('Hello'):
+  print("i, char:", i, char) # 0 H, 1 e, 2 l ....
+
+print(":", ) # 
+
+for i, number in enumerate([1,2,3]):
+  print("i, char:", i, number)
+
+print(":", ) # 
