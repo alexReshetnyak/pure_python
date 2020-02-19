5ea06a00951fffd61c870413833844ef9431c559
AReshetnyak <alexreshetnyakmail@gmail.com>
2020-02-19 10:11:33 +0200

    learned more about functions

--- 1_fundamentals/18_functions2.py
+++ 1_fundamentals/18_functions2.py
@@ -1,6 +1,6 @@
 print('---------------------------FUNCTIONS 2--------------------------------')
 print('---------------------------METHODS VS FUNCTIONS--------------------------------')
-# ? Methods alway belong to objects (car.drive)
+# * Methods alway belong to objects (car.drive)
 
 print('---------------------------DOC STRINGS--------------------------------')
 
@@ -25,8 +25,8 @@ print("is_even(50):", is_even(50)) # True
 
 
 print('---------------------------*args and **kwargs (key word args)--------------------------------')
-# ? *args - make tuple from parameters
-# ? **kwargs - make dictionary from keyword arguments
+# * *args - make tuple from parameters
+# * **kwargs - make dictionary from keyword arguments
 def super_func(*args, **kwargs):
   print("args:", args) # (1, 2, 3, 4, 5)
   print("kwargs:", kwargs) #  {'num1': 5, 'num2': 10}
@@ -38,7 +38,7 @@ def super_func(*args, **kwargs):
 print("super_func(1,2,3,4,5):", super_func(1,2,3,4,5, num1=5, num2=10)) # 30
 
 # ---------------------------------------------------------------------------------
-# ? Rule oreder: params, *args, default_parametes, **kwargs
+# * Rule oreder: params, *args, default_parametes, **kwargs
 def super_func2(name, *args, i='hi', **kwargs):
   print("name:", name) # 1
   print("i:", i) # hi
@@ -49,5 +49,31 @@ def super_func2(name, *args, i='hi', **kwargs):
 
 print("super_func2(1,2,3,4,5):", super_func2(1,2,3,4,5, num1=5, num2=10))
 
-print(":", ) #
+
+print('---------------------------FUNCTION AS PARAMETER--------------------------------')
+# * Pass function as parameter
+def wrap(func, *args):
+  return func(args[0])
+
+wrap(test, 'Say hello!') # Say hello!
+
+
+print('---------------------------EXERCISE--------------------------------')
+
+def highest_even(li):
+  biggest_number = 0
+  for number in li:
+    if number > biggest_number and number % 2 == 0:
+      biggest_number = number
+  return biggest_number
+
+
+def highest_even2(li):
+  evens = []
+  for item in li:
+    if item % 2 == 0:
+      evens.append(item)
+  return max(evens)
+
+print("highest_even2([10,2,3,4,8]):", highest_even2([10,2,3,4,8])) # 10
 print(":", ) #
---
+++ 1_fundamentals/19_scope.py
@@ -0,0 +1,94 @@
+print('---------------------------SCOPE--------------------------------')
+# * Scope - what variables do I have access to?
+total = 100 # * creates variable in global scope
+
+# ------------------------------------------------------
+# * Python has function scope (every new function creates it own scope)
+def do_something():
+  data = 'data'
+
+# print("data:", data) # !  name 'data' is not defined
+
+# ------------------------------------------------------
+# * if, while, for and etc don't create new scope
+if True:
+  a = 'a'
+print("a:", a) # a
+
+
+print('---------------------------SCOPE RULES--------------------------------')
+# * 1 - start search with local scope
+# * 2 - then look into Parent scope?
+# * 3 - look in global
+# * 4 - check if it built in function (like max, sum and etc)
+a = 1
+def confusion():
+  a = 5   # * creates it one variable
+  return a
+print("confusion(), a:", confusion(), a) # 5, 1
+
+# -------------------------------------------------------
+a = 1
+def parent():
+  a = 10
+  def confusion():
+    return a
+  a = 11
+  return confusion()
+
+print("parent():", parent()) # 11
+
+
+print('---------------------------GLOBAL KEYWORD--------------------------------')
+def confusion2(b):
+  # * Creates b in local scope
+  print(b)
+
+print("confusion():", confusion2(300)) # 300
+
+# ---------------------------------------------
+total = 0
+
+def count():
+  # * get total from global scope
+  # ! bad practice to use global
+  global total
+  total += 1
+  return total
+print("count():", count()) # 1
+print("count():", count()) # 2
+
+# ------------------------------------------
+# * DI
+total = 0
+def count2(total):
+  total += 1
+  return total
+total = count2(total)
+total = count2(total)
+print("total:", total) # 2
+
+
+print('---------------------------NONLOCAL KEYWORD--------------------------------')
+
+def outer():
+  x = 'local'
+  def inner():
+    # * get variable from parent scope
+    # ! doesn't work if parent scope is global
+    nonlocal x
+    x = 'nonlocal'
+    print('inner x:', x) # nonlocal
+
+  inner()
+  print('outher x:', x) # nonlocal
+
+outer()
+
+print(":", ) #
+print(":", ) #
+print(":", ) #
+print(":", ) #
+print(":", ) #
+print(":", ) #
+print(":", ) #
